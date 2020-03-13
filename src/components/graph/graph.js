4ccbd51a7e62af11d8406ee0ed1837d037840861
Nick Holke <nickholke@gmail.com>
2020-03-12 19:53:43 -0700

    Added graph animation

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,37 +1,30 @@
 import React from 'react';
 import { 
   graphWrapper,
-  barWrapper,
-  category,
-  barBackground,
-  bar,
-  bar1,
-  barGrow, 
+  show,
 } from './graph.module.scss';
-import cx from 'classnames';
 import { useInView } from 'react-intersection-observer';
 import { Bar } from '../routes';
 
 const Graph = () => {
-  // const [ref, inView, entry] = useInView({
-  //   /* Optional options */
-  //   threshold: 0,
-  // });
+  const [ref, inView, entry] = useInView({
+    threshold: 0,
+  });
 
-  // if (inView) {
-  //   entry.target.children[1].children[0].classList.add(barGrow);
-  // }
+  if (inView) {
+    entry.target.classList.add(show)
+  }
 
   return (
-    <div className={graphWrapper}>
-      <Bar width='90%' delay='200ms' name='REACT'/>
-      <Bar width='90%' delay='300ms' name='JAVASCRIPT'/>
-      <Bar width='80%' delay='400ms' name='NATIVE'/>
-      <Bar width='80%' delay='500ms' name='CSS'/>
-      <Bar width='80%' delay='600ms' name='HTML'/>
-      <Bar width='75%' delay='700ms' name='NODE'/>
-      <Bar width='70%' delay='800ms' name='MySQL'/>
-      <Bar width='70%' delay='900ms' name='Mongo'/>
+    <div ref={ref} className={graphWrapper}>
+      <Bar width='90%' delay='900ms' name='REACT'/>
+      <Bar width='90%' delay='1000ms' name='JAVASCRIPT'/>
+      <Bar width='80%' delay='1100ms' name='NATIVE'/>
+      <Bar width='80%' delay='1200ms' name='CSS'/>
+      <Bar width='80%' delay='1300ms' name='HTML'/>
+      <Bar width='75%' delay='1400ms' name='NODE'/>
+      <Bar width='70%' delay='1500ms' name='MySQL'/>
+      <Bar width='70%' delay='1600ms' name='Mongo'/>
     </div>
   );
 }
