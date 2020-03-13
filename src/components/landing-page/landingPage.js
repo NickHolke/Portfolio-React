a6893448865aae455395f4a51c6edbeaccd345ff
Nick Holke <nickholke@gmail.com>
2020-03-13 15:05:36 -0700

    Animated landing page

--- src/components/landing-page/landingPage.js
+++ src/components/landing-page/landingPage.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import { landingPage, topHalf, bottomHalf } from './landingPage.module.scss';
+import { 
+  landingPage, 
+  topHalf, 
+  bottomHalf,
+  topText,
+  bottomText,
+  showPage,
+  moveText, 
+} from './landingPage.module.scss';
+import { useInView } from 'react-intersection-observer';
 
-const LandingPage = () => (
-  <div className={landingPage}>
-    <div className={topHalf}>
-      <h1>Hello I'm Nick Holke.</h1>
+const LandingPage = () => {
+  const [ref, inView, entry] = useInView({
+    threshold: 0,
+  })
+
+  if (inView) {
+    for (let i = 0; i < 2; i++) {
+      let child = entry.target.children[i];
+      child.classList.add(showPage);
+      child.children[0].classList.add(moveText);
+    }
+  }
+
+  return (
+    <div ref={ref} className={landingPage}>
+      <div className={topHalf}>
+        <h1 class={topText}>Hello I'm Nick Holke.</h1>
+      </div>
+      <div className={bottomHalf}>
+        <h1 class={bottomText}>I'm a software engineer.</h1>
+        V
+      </div>
     </div>
-    <div className={bottomHalf}>
-      <h1>I'm a software engineer.</h1>
-      V
-    </div>
-  </div>
-)
+  )
+}
 
 export default LandingPage;
