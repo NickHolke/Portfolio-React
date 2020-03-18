39236f266caea157004e2d4716d581215323c162
Nick Holke <nickholke@gmail.com>
2020-03-18 14:15:31 -0700

    Updates

---
+++ precache-manifest.5a24ac74eb6a5231cb482202340937cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "981ad1db16d110bb009a5e8d2351191e",
+    "url": "/Portfolio-React/index.html"
+  },
+  {
+    "revision": "fe1b5353351ea4937c81",
+    "url": "/Portfolio-React/static/css/main.6c20b246.chunk.css"
+  },
+  {
+    "revision": "a5036dac86916ad08267",
+    "url": "/Portfolio-React/static/js/2.615fe762.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Portfolio-React/static/js/2.615fe762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe1b5353351ea4937c81",
+    "url": "/Portfolio-React/static/js/main.83fb1399.chunk.js"
+  },
+  {
+    "revision": "c0230294af0ac6160f72",
+    "url": "/Portfolio-React/static/js/runtime-main.9ae2701b.js"
+  }
+]);
