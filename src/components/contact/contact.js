2b5b660cf7afb9ffe2f4c3914395b6587d13c8b5
Nick Holke <nickholke@gmail.com>
2020-03-19 20:39:37 -0700

    Formatted contact section and attempted emailjs

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import styles from './contact.module.scss';
+import { formWrapper, wrapper, title, titleText, input, submit } from './contact.module.scss';
+import * as emailjs from 'emailjs-com';
 
 const Contact = () => {
   const [name, setName] = useState('');
@@ -7,33 +8,70 @@ const Contact = () => {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
+  const resetHooks = () => {
+    setName('');
+    setSubject('');
+    setEmail('');
+    setMessage('');
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    let templateParams = {
+      from_name: email,
+      to_name: 'nickholke@gmail.com',
+      subject: subject,
+      message_html: message,
+    }
+
+    emailjs.send(
+      'gmail',
+      'template_z4G7eMor',
+      templateParams,
+      'user_h4jBBfPVyh1aKLrWcGfuV',
+    )
+
+    resetHooks();
+  }
+
+
   return (
-    <form>
-      <input 
-        type="text" 
-        value={name} 
-        onChange={(e) => setName(e.target.value)}
-        placeholder='Enter Name'
-      />
-      <input 
-        type="email" 
-        value={email} 
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder='Enter Email'
-      />
-      <input 
-        type="text" 
-        value={subject} 
-        onChange={(e) => setSubject(e.target.value)}
-        placeholder='Enter Subject'
-      />
-      <textarea
-        type="textarea" 
-        value={message} 
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder='Enter Message'
-      />
-    </form>
+    <div className={wrapper}>
+      <div className={title}>
+        <h2 className={titleText}>Contact</h2>
+      </div>
+      <form className={formWrapper} onSubmit={submitHandler}>
+        <input
+          className={input} 
+          type="text" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+          placeholder='Enter Name'
+        />
+        <input
+          className={input}  
+          type="email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Enter Email'
+        />
+        <input
+          className={input}  
+          type="text" 
+          value={subject} 
+          onChange={(e) => setSubject(e.target.value)}
+          placeholder='Enter Subject'
+        />
+        <textarea
+          className={input} 
+          type="textarea" 
+          value={message} 
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder='Enter Message'
+        />
+        <input className={submit} type='submit' value='SUBMIT'/>
+      </form>
+    </div>
   )
 }
 
