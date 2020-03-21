c6bcf7b062554893afad49d2e77db1517dcc793b
Nick Holke <nickholke@gmail.com>
2020-03-21 16:28:16 -0700

    fixed video showing bug

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import {title, portfolioSection, projectsWrapper, videoWrapper, backgroundStuff,} from './portfolio.module.scss';
+import {title, portfolioSection, projectsWrapper, videoWrapper, bgColor, bgShow, showWrapper} from './portfolio.module.scss';
 import {data} from './mockData';
 import Project from '../project/project';
 import { useInView } from 'react-intersection-observer';
+import classNames from 'classnames';
 
 const Portfolio = (props) => {
   const [projects, setProjects] = useState(data);
   const [showProjects, setShowProjects] = useState(false);
   const [ref, inView, entry] = useInView();
   const [video, setVideo] = useState('');
+  const [videoClass, setVideoClass] = useState(videoWrapper);
+  const [bgClass, setBgClass] = useState(bgColor);
   const [showVideo, setShowVideo] = useState(false);
   const [showControls, setShowControls] = useState(false);
 
@@ -23,16 +26,28 @@ const Portfolio = (props) => {
     }
   }, [inView])
 
+  useEffect(() => {
+    if (showVideo) {
+      setTimeout(() => {
+        setVideoClass(classNames(videoWrapper, showWrapper));
+        setBgClass(classNames(bgColor, bgShow));
+      }, 50)
+    } else {
+      setVideoClass(videoWrapper)
+      setBgClass(bgColor);
+    }
+  }, [showVideo])
+
   return (
     <div id="portfolio-section" className={portfolioSection}>
       {showVideo &&
       <>
-        <div className={videoWrapper}>
+        <div className={videoClass}>
           <video controls={showControls} onLoadedData={() => setShowControls(true)}>
             <source src={video} type="video/mp4"/>
           </video>
         </div>
-      <div className={backgroundStuff} onClick={()=>setShowVideo(false)}>
+      <div className={bgClass} onClick={()=>setShowVideo(false)}>
       </div>
       </>
       }
