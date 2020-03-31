ef9893d3c3bb470354809c9c2dc1edea1f4ce9e3
Nick Holke <nickholke@gmail.com>
2020-03-31 14:59:16 -0700

    Added react helmet for head

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.scss';
 import { About, LandingPage, Portfolio, NavBar, Contact } from '../routes'
+import {Helmet} from "react-helmet";
+import mediaImage from './media_img.png';
 
 function App() {
   return (
     <>
+      <Helmet>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Nick Holke Portfolio"/>
+        <meta property="og:title" content="Nick Holke"/>
+        <meta property='og:description' content="An online portfolio."/>
+        <meta property="og:image" content={mediaImage}/>
+        <meta property="og:url" content="https://www.nickholke.com/"/>
+        {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
+        <link rel="icon" href="./Nick_favicon.png" />
+        <title>Nick Holke</title>
+      </Helmet>
       <LandingPage />
       <NavBar />
       <About />
